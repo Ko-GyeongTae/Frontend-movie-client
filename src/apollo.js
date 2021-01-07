793f530512e4ce31264fe4d2b6fe066446e618ee
Ko-GyeongTae <kokt0203@naver.com>
2021-01-07 23:58:41 +0900

    last commit

--- src/apollo.js
+++ src/apollo.js
@@ -1,6 +1,6 @@
 import ApolloClient from "apollo-boost";
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://apollomovie.herokuapp.com",
   resolvers: {
     Movie: {
       isLiked: () => false
